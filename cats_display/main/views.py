7f5e74791ab93f7b1672cae14ccf4a36d247bdcf
Obaid <ephiram2002@gmail.com>
2022-08-24 03:13:35 +0530

    Completed DRF of cat service 1

--- cats_display/main/views.py
+++ cats_display/main/views.py
@@ -2,20 +2,14 @@ from django.shortcuts import render
 import random
 import requests
 import json
-import os
-
-catsList = []
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html')
 
 def get_cat_photos(request):
-    global catsList
-    if(len(catsList) < 3):
-        url ="https://api.thecatapi.com/v1/images/search?api_key=YOUR_API_KEY"
-        r = requests.get(url)
-        data = json.loads(r.text)
-        catsList.append(data[0]['url'])
-    context = {'cat_photo_url': random.choice(catsList)}
+    url = "http://127.0.0.1:8001/"
+    r = requests.get(url)
+    data = json.loads(r.text)
+    context = {'cat_photo_url': data['url']}
     return render(request, 'cat_photos.html', context)
---
+++ cats_service1/main/views.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+import requests
+import json
+import os
+
+# Create your views here.
+
+catPhotoUrl = None
+
+class CatPhotoSerializer(serializers.Serializer):
+    url = serializers.CharField()
+
+@api_view(['GET'])
+def index(request):
+    global catPhotoUrl
+    if not catPhotoUrl:
+        url ="https://api.thecatapi.com/v1/images/search?api_key=YOUR_CAT_API_KEY"
+        r = requests.get(url)
+        data = json.loads(r.text)
+        catPhoto = {'url': data[0]['url']}
+        catPhotoUrl = CatPhotoSerializer(catPhoto).data
+    return Response(catPhotoUrl)
